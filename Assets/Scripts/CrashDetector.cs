e202a08a92c82a2434cd8dedaa6f0055d6328feb
Dimas Bayu Nugraha <dimasbayuworks@gmail.com>
2022-06-27 21:32:47 +0700

    add finish and crash detector

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Ground")
+        {
+            Debug.Log("Yu ded");
+            SceneManager.LoadScene(0);
+        }
+    }
+}
